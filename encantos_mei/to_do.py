0d5de7e52242bd65d11547d5734fc66d845f07c4
Tilo González <tkerrygan@gmail.com>
2024-10-13 14:19:39 -0300

    Se actualizó reqs y collectstatic

--- encantos_mei/to_do.py
+++
@@ -1,7 +0,0 @@
-* Arreglar footer: agregar íconos, ver bien qué páginas agregar y en qué estilos.
-* Agregar /pages: completar con todas las páginas faltantes.
-* Colocar placeholder formulario search navbar.
-arreglar borde botones Eliminar de los formularios
-Reformatear formularios
-Agregar funcionalidad cambiar contraseña
-Agregar servicio
